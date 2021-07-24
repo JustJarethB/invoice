65973b1a8526ba8560dfae26031385019616d28b
Jareth <31932204+JustJarethB@users.noreply.github.com>
2021-07-24 18:59:18 +0100

    rename meta

--- src/components/InvoiceMeta.js
+++ src/components/InvoiceMeta.js
@@ -5,11 +5,11 @@ export default ({ id, date, purchaseOrder, onChange }) => (
     <div className="p-2">
         <div className="p-2 w-full ring-4 ring-gray-300 rounded-sm">
             <div className="flex items-center">
-                <p className="font-bold text-lg px-2">Invoice #</p>
+                <p className="font-bold text-lg px-2">Invoice Ref</p>
                 <TextInput value={id} onChange={v => onChange({ id: v })} />
             </div>
             <div className="flex items-center">
-                <p className="font-bold text-lg px-2">Date</p>
+                <p className="font-bold text-lg px-2">Tax Date</p>
                 <TextInput value={date} onChange={v => onChange({ date: v })} />
             </div>
             <div className="flex items-center">
