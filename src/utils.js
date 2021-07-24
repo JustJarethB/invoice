fd0a2fc8b79b6f919e7cb9b9372715e004d67de6
Jareth <31932204+JustJarethB@users.noreply.github.com>
2021-07-24 19:37:11 +0100

    allow negative numbers

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,13 @@
 export const formatCurrency = v => Number.isNaN(v) ? 0 : Number(v).toFixed(2);
-export const ensureFutureCurrency = v => (Number.isNaN(parseFloat(v))) ? undefined : (v.toString().match(/0*(\d+\.?\d{0,2})/)[1] || undefined);
+export const ensureFutureCurrency = v => {
+    let result;
+    const s = v.toString();
+    if (/-/.test(s)) result = "-";
+    if (Number.isNaN(parseFloat(v))) return result;
+    result = result || "";
+    const regRes = s.match(/0*(\d+\.?\d{0,2})/);
+    const regMatch = regRes[1] || undefined;
+    result += regMatch;
+    return result;
+}
+// const regRes = s.match(/0*(-?\d+\.?\d{0,2})/);
