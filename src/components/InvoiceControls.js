e7506dc2c96007eccf9d0e3490ea18bdafaf0ef9
Jareth <31932204+JustJarethB@users.noreply.github.com>
2021-07-27 18:49:09 +0100

    refactor add Button

--- src/components/InvoiceControls.js
+++ src/components/InvoiceControls.js
@@ -4,20 +4,17 @@ export default ({ loadClientAddress }) => (
     <div className="print:hidden sticky top-0 bg-gray-50 shadow-sm z-10">
         <div className="container mx-auto">
             <div className="w-full flex justify-end">
-                <div className="p-2">
-                    <button className="p-2 px-4 rounded-lg hover:bg-gray-600 ring-2 ring-white hover:ring-gray-900 bg-gray-700 text-white font-bold" type="button" onClick={() => loadClientAddress(0)}>SSW</button>
-                </div>
-                <div className="p-2">
-                    <button className="p-2 px-4 rounded-lg hover:bg-gray-600 ring-2 ring-white hover:ring-gray-900 bg-gray-700 text-white font-bold" type="button" onClick={() => loadClientAddress(1)}>TBNUK</button>
-                </div>
-                <div className="p-2">
-                    <button className="p-2 px-4 rounded-lg hover:bg-gray-600 ring-2 ring-white hover:ring-gray-900 bg-gray-700 text-white font-bold" type="button" onClick={window.print}>Print</button>
-                </div>
-                <div className="p-2">
-                    <button className="p-2 px-4 rounded-lg hover:bg-gray-600 ring-2 ring-white hover:ring-gray-900 bg-gray-700 text-white font-bold" type="button" onClick={null}>Save</button>
-                </div>
+                <Button onClick={() => loadClientAddress(0)}>SSW</Button>
+                <Button onClick={() => loadClientAddress(1)}>TBNUK</Button>
+                <Button onClick={window.print}>Print</Button>
+                <Button>Save</Button>
             </div>
         </div>
     </div>
 )
 
+const Button = ({ children, onClick }) => (
+    <div className="p-2">
+        <button className="p-2 px-4 rounded-lg hover:bg-gray-600 ring-2 ring-white hover:ring-gray-900 bg-gray-700 text-white font-bold" type="button" onClick={onClick}>{children}</button>
+    </div>
+)
