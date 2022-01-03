2a98a03edbab6b87cc33656e3927ac86f01e7cba
Jareth <31932204+JustJarethB@users.noreply.github.com>
2022-01-03 13:49:06 +0000

    241 change address, update db saving

--- src/components/Address.js
+++ src/components/Address.js
@@ -6,7 +6,9 @@ export default ({ address, onChange, className = 0, title }) => (
         <div className="w-full ring-4 ring-gray-300 rounded-sm p-2">
             <h2>{title}</h2>
             <TextInput value={address.name} className="text-2xl pb-8 w-full px-2 font-bold" placeholder="Name" onChange={v => onChange({ name: v })} />
-            <TextInput value={address.address} className="w-full px-2 font-bold" placeholder="Address" onChange={v => onChange({ address: v })} />
+            <TextInput value={address.streetAddress} className="w-full px-2 font-bold" placeholder="Street Address" onChange={v => onChange({ streetAddress: v })} />
+            <TextInput value={address.city} className="w-full px-2 font-bold" placeholder="City/Town" onChange={v => onChange({ city: v })} />
+            <TextInput value={address.county} className="w-full px-2 font-bold" placeholder="County" onChange={v => onChange({ county: v })} />
             <TextInput value={address.postCode} className="w-full px-2 font-bold" placeholder="Postcode" onChange={v => onChange({ postCode: v })} />
         </div>
     </div>
