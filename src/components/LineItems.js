14b2c77f2b06d4e09ed1e820ecc3e32eda5efd2e
Jareth <31932204+JustJarethB@users.noreply.github.com>
2022-01-08 17:12:35 +0000

    update LineItem from flex to table

--- src/components/LineItems.js
+++ src/components/LineItems.js
@@ -18,19 +18,19 @@ const lineUnitOptions = [
 // const getVatRates = () => ([{ label: "20", value: .2 }, { label: "5", value: .05 }, { label: "N/A", value: 0 }])
 
 const LineItems = ({ data, onChange }) => (
-    <div className="p-2">
-        <div className="pb-2 flex">
-            <div className="p-2 w-1/6 font-bold">Service Date</div>
-            <div className="p-2 w-1/12 print:hidden font-bold">Type</div>
-            <div className="p-2 w-1/6 font-bold">Ref</div>
-            <div className="p-2 w-1/6 font-bold">Unit</div>
-            <div className="p-2 w-1/12 font-bold">Quantity</div>
-            <div className="p-2 w-1/6 font-bold">Unit Price</div>
-            {/* <div className="p-2 w-1/6 font-bold">VAT Rate</div>
-            <div className="p-2 w-1/6 font-bold">VAT</div> */}
-            <div className="p-2 w-1/6 font-bold">Total</div>
+    <div className="p-2 table">
+        <div className="pb-2 table-row">
+            <div className="p-2 table-cell font-bold">Service Date</div>
+            <div className="p-2 table-cell print:hidden font-bold">Type</div>
+            <div className="p-2 table-cell font-bold">Ref</div>
+            <div className="p-2 table-cell font-bold">Unit</div>
+            <div className="p-2 table-cell font-bold">Quantity</div>
+            <div className="p-2 table-cell font-bold">Unit Price</div>
+            {/* <div className="p-2 table-cell font-bold">VAT Rate</div>
+            <div className="p-2 table-cell font-bold">VAT</div> */}
+            <div className="p-2 table-cell font-bold">Total</div>
         </div>
-        <div className="ring-4 ring-gray-300 rounded-sm">
+        <div className="ring-4 ring-gray-300 rounded-sm table-row-group">
             {/* Need to give key that doesn't change with UserInput, can't use index either */}
             {data.map((l, i) => <LineItem index={i} item={l} onChange={d => onChange({ id: i, data: d })} className={(i === data.length - 1) ? "print:hidden" : ""} />)}
         </div>
@@ -39,26 +39,26 @@ const LineItems = ({ data, onChange }) => (
 
 
 const LineItem = ({ index, item = {}, onChange, className }) => (
-    <div className={`flex ${index % 2 && "bg-gray-100"} print:text-xs ${className}`} >
-        <div className="p-1 w-1/6">
+    <div className={`flex ${index % 2 && "bg-gray-100"} table-row print:text-xs ${className}`} >
+        <div className="p-1 table-cell ">
             <DateInput className="w-full font-bold" value={item.date} onChange={(v) => onChange({ date: v })} />
         </div>
-        <div className="p-1 print:hidden w-1/12">
+        <div className="p-1 table-cell print:hidden ">
             <SelectInput options={lineTypeOptions} className="w-full font-bold" value={item.type} onChange={(v) => onChange({ type: v })} />
         </div>
-        <div className="p-1 w-1/6">
+        <div className="p-1 table-cell ">
             <TextInput className="w-full font-bold" value={item.ref} onChange={(v) => onChange({ ref: v })} />
         </div>
-        <div className="p-1 w-1/6">
+        <div className="p-1 table-cell ">
             <SelectInput options={lineUnitOptions} className="w-full font-bold" value={item.name} onChange={(v) => onChange({ name: v })} />
         </div>
-        <div className="p-1 w-1/12">
+        <div className="p-1 table-cell ">
             <TextInput className="w-full font-bold" value={item.qty} onChange={(v) => onChange({ qty: v })} />
         </div>
-        <div className="p-1 w-1/6">
+        <div className="p-1 table-cell ">
             <TextInput className="w-full font-bold" prefix="£" value={(item.unitPrice)} onChange={(v) => onChange({ unitPrice: ensureFutureCurrency(v) })} />
         </div>
-        <div className="p-1 w-1/6">
+        <div className="p-1 table-cell ">
             <TextInput className="w-full font-bold" prefix="£" value={formatCurrency(item.qty * item.unitPrice) || undefined} />
         </div>
     </div>
